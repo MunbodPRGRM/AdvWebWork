5fe72e9b152a04271543b20db4a199a9330e6e2a
MunbodPRGRM <Natakrit120@gmail.com>
2025-07-07 16:35:46 +0700

    create travel and link to app

--- app-hw1/angular-ui/src/app/app.ts
+++ app-hw1/angular-ui/src/app/app.ts
@@ -1,10 +1,12 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { Travel } from './components/travel/travel';
 
 @Component({
   selector: 'app-root',
   imports: [
-    RouterOutlet
+    RouterOutlet, 
+    Travel
   ],
   templateUrl: './app.html',
   styleUrl: './app.scss'
